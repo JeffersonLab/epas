f744fe6a0c6798bb5a5cdf11ba8f25b8617e1edc
theo <theo@jlab.org>
2021-04-20 09:16:24 -0400

    fix output file location

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 
 let mix = require('laravel-mix');
 
-mix.setPublicPath('public')
+mix.setPublicPath('public/vendor/jlab-epas')
     .js('resources/js/app.js', 'js/')
     .sass('resources/css/app.scss', 'css/')
     .webpackConfig({
